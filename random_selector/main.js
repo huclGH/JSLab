c8c6b6b9951a586b42d272db5f8463841dd349f9
huclGH <clhu@outlook.com>
2021-05-06 16:17:41 +0800

    Fourth commit: Random Selector bug fixed

--- random_selector/main.js
+++ random_selector/main.js
@@ -3,12 +3,12 @@ $(function () {
         //alert("Hi");
         var numberOfListItem=$("#foodchoices li").length;
         var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
-        $("h1").text($("foodchoices li").eq(randomChildNumber).text());
+        $("h1").text($("#foodchoices li").eq(randomChildNumber).text());
         //想要找到img元件
         //設定img元件的SRC屬性 ->對應元件的屬性
         //用同一數字的
         //jQuery set attributes 
-        $("img").attr("src",$("foodchoices li").eq(randomChildNumber).attr("title"))
+        $("img").attr("src",$("#foodchoices li").eq(randomChildNumber).attr("title"))
         
 
 
